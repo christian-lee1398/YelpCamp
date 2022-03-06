dcdc707e7230640a5cafe1e8b01484e3ef846b1b
Christian Lee <christian.lee1398@gmail.com>
2022-03-06 16:06:00 -0500

    fix seeds index.js

--- seeds/index.js
+++ seeds/index.js
@@ -29,7 +29,7 @@ const seedDB = async () => {
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             //YOUR USER ID
-            author: '61cb7fe81779a3e91586a5e1',
+            author: "61fc1d6956eb094dde3ac561",
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
